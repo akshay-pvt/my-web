956618a9b0fe77f5f6c29eca3acb6d7a7a4c39b6
akshay-pvt <akshaydev497@gmail.com>
2026-02-24 21:08:32 +0530

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.disable('x-powered-by');
+
+app.get ('/', (req, res)=> {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+app.user((req, res => {
+  res.status(403).send('umb');
+});
+module.exports = app;
